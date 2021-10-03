2360999b2001609b3edc28957e506c5884186283
seemazanvar <srzanvar_dcd@jspm.edu.in>
2021-10-03 11:48:13 +0530

    Angular Http Observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BatchesListComponent } from './batches-list/batches-list.component';
+import { BatchDetailsComponent } from './batch-details/batch-details.component';
+import { BatchService } from './batch.service'; // Import the required service
+
+// Import httpclientmodule
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BatchesListComponent,
+    BatchDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule    // Add the name into import
   ],
-  providers: [],
+
+  providers: [BatchService],  // Register name of our service
   bootstrap: [AppComponent]
 })
 export class AppModule { }
